e681a07cfea0c0d9650e1911846bec70cfde99cc
Oskar Maier <oskar.maier@googlemail.com>
2014-05-09 12:26:36 +0200

    Added some more pre-defined sets to the configuration files and made their handling easier.

--- featureconfig.py
+++ featureconfig.py
@@ -5,7 +5,7 @@
 from medpy.features.intensity import intensities, centerdistance, centerdistance_xdminus1, local_mean_gauss, local_histogram, hemispheric_difference, median, guassian_gradient_magnitude
 from medpy.features.intensity import indices as indices_feature
 
-_features_to_extract = [
+_flair = [
 	('flair_tra', intensities, [], False),
 	('flair_tra', local_mean_gauss, [3], True),
 	('flair_tra', local_mean_gauss, [5], True),
@@ -20,19 +20,66 @@ _features_to_extract = [
 	('flair_tra', centerdistance_xdminus1, [2], True)
 ]
 
-features_to_extract = [
+_dw = [
+	('dw_tra_b1000_dmean', intensities, [], False),
+	('dw_tra_b1000_dmean', local_mean_gauss, [3], True),
+	('dw_tra_b1000_dmean', local_mean_gauss, [5], True),
+	('dw_tra_b1000_dmean', local_mean_gauss, [7], True),
+	#('dw_tra_b1000_dmean', guassian_gradient_magnitude, [5], True),
+	#('dw_tra_b1000_dmean', median, [7], True),
+	('dw_tra_b1000_dmean', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
+	('dw_tra_b1000_dmean', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
+	('dw_tra_b1000_dmean', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
+	#('dw_tra_b1000_dmean', centerdistance_xdminus1, [0], True),
+	#('dw_tra_b1000_dmean', centerdistance_xdminus1, [1], True),
+	#('dw_tra_b1000_dmean', centerdistance_xdminus1, [2], True)
+]
+
+_adc = [
+	('adc_tra', intensities, [], False),
+	('adc_tra', local_mean_gauss, [3], True),
+	('adc_tra', local_mean_gauss, [5], True),
+	('adc_tra', local_mean_gauss, [7], True),
+	#('adc_tra', guassian_gradient_magnitude, [5], True),
+	#('adc_tra', median, [7], True),
+	('adc_tra', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
+	('adc_tra', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
+	('adc_tra', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
+	#('adc_tra', centerdistance_xdminus1, [0], True),
+	#('adc_tra', centerdistance_xdminus1, [1], True),
+	#('adc_tra', centerdistance_xdminus1, [2], True)
+]
+
+_t1 = [
 	('t1_sag_tfe', intensities, [], False),
 	('t1_sag_tfe', local_mean_gauss, [3], True),
 	('t1_sag_tfe', local_mean_gauss, [5], True),
 	('t1_sag_tfe', local_mean_gauss, [7], True),
-	#('flair_tra', guassian_gradient_magnitude, [5], True),
-	#('flair_tra', median, [7], True),
+	#('t1_sag_tfe', guassian_gradient_magnitude, [5], True),
+	#('t1_sag_tfe', median, [7], True),
 	('t1_sag_tfe', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
 	('t1_sag_tfe', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
-	('t1_sag_tfe', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False), #11 bins, 15*2=30mm region
-	('t1_sag_tfe', centerdistance_xdminus1, [0], True),
-	('t1_sag_tfe', centerdistance_xdminus1, [1], True),
-	('t1_sag_tfe', centerdistance_xdminus1, [2], True)
+	('t1_sag_tfe', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
+	#('t1_sag_tfe', centerdistance_xdminus1, [0], True),
+	#('t1_sag_tfe', centerdistance_xdminus1, [1], True),
+	#('t1_sag_tfe', centerdistance_xdminus1, [2], True)
 ]
 
-#t1_sag_tfe
+_t2 = [
+	('t2_sag_tse', intensities, [], False),
+	('t2_sag_tse', local_mean_gauss, [3], True),
+	('t2_sag_tse', local_mean_gauss, [5], True),
+	('t2_sag_tse', local_mean_gauss, [7], True),
+	#('t2_sag_tse', guassian_gradient_magnitude, [5], True),
+	#('t2_sag_tse', median, [7], True),
+	('t2_sag_tse', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
+	('t2_sag_tse', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
+	('t2_sag_tse', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
+	#('t2_sag_tse', centerdistance_xdminus1, [0], True),
+	#('t2_sag_tse', centerdistance_xdminus1, [1], True),
+	#('t2_sag_tse', centerdistance_xdminus1, [2], True)
+]
+
+features_to_extract = _flait + _t2
+
+
