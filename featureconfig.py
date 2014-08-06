6793577a247e3e9ba488d8cd6072ef65f0d52866
Oskar Maier <oskar.maier@googlemail.com>
2014-08-06 10:34:44 +0200

    Adapted the feature config to the UKE MS case.

--- featureconfig.py
+++ featureconfig.py
@@ -5,106 +5,59 @@
 from medpy.features.intensity import intensities, centerdistance, centerdistance_xdminus1, local_mean_gauss, local_histogram, hemispheric_difference, median, guassian_gradient_magnitude, shifted_mean_gauss
 from medpy.features.intensity import indices as indices_feature
 
-_flair = [
-	('flair_tra', intensities, [], False),
-	#('flair_tra', shifted_mean_gauss, [(2, 2, 2), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(2, 2, -2), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(2, -2, 2), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-2, 2, 2), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(2, -2, -2), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-2, 2, -2), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-2, -2, 2), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-2, -2, -2), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(1, 1, 1), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(1, 1, -1), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(1, -1, 1), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-1, 1, 1), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(1, -1, -1), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-1, 1, -1), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-1, -1, 1), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-1, -1, -1), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(4, 4, 4), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(4, 4, -4), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(4, -4, 4), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-4, 4, 4), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(4, -4, -4), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-4, 4, -4), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-4, -4, 4), 3], True),
-	#('flair_tra', shifted_mean_gauss, [(-4, -4, -4), 3], True),
-	('flair_tra', local_mean_gauss, [3], True),
-	('flair_tra', local_mean_gauss, [5], True),
-	('flair_tra', local_mean_gauss, [7], True),
-	#('flair_tra', guassian_gradient_magnitude, [5], True),
-	#('flair_tra', median, [7], True),
-	#('flair_tra', indices_feature, [], True),
-	('flair_tra', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
-	('flair_tra', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
-	('flair_tra', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False), #11 bins, 15*2=30mm region
-	('flair_tra', centerdistance_xdminus1, [0], True),
-	('flair_tra', centerdistance_xdminus1, [1], True),
-	('flair_tra', centerdistance_xdminus1, [2], True)
+T2 = [
+	('T2', intensities, [], False),
+	('T2', local_mean_gauss, [3], True),
+	('T2', local_mean_gauss, [5], True),
+	('T2', local_mean_gauss, [7], True),
+	('T2', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
+	('T2', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
+	('T2', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False), #11 bins, 15*2=30mm region
+	('T2', centerdistance_xdminus1, [0], True),
+	('T2', centerdistance_xdminus1, [1], True),
+	('T2', centerdistance_xdminus1, [2], True)
 ]
 
-_dw = [
-	('dw_tra_b1000_dmean', intensities, [], False),
-	('dw_tra_b1000_dmean', local_mean_gauss, [3], True),
-	('dw_tra_b1000_dmean', local_mean_gauss, [5], True),
-	('dw_tra_b1000_dmean', local_mean_gauss, [7], True),
-	#('dw_tra_b1000_dmean', guassian_gradient_magnitude, [5], True),
-	#('dw_tra_b1000_dmean', median, [7], True),
-	('dw_tra_b1000_dmean', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
-	('dw_tra_b1000_dmean', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
-	('dw_tra_b1000_dmean', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
-	#('dw_tra_b1000_dmean', centerdistance_xdminus1, [0], True),
-	#('dw_tra_b1000_dmean', centerdistance_xdminus1, [1], True),
-	#('dw_tra_b1000_dmean', centerdistance_xdminus1, [2], True)
+FLAIR = [
+	('FLAIR', intensities, [], False),
+	('FLAIR', local_mean_gauss, [3], True),
+	('FLAIR', local_mean_gauss, [5], True),
+	('FLAIR', local_mean_gauss, [7], True),
+	('FLAIR', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
+	('FLAIR', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
+	('FLAIR', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
 ]
 
-_adc = [
-	('adc_tra', intensities, [], False),
-	('adc_tra', local_mean_gauss, [3], True),
-	('adc_tra', local_mean_gauss, [5], True),
-	('adc_tra', local_mean_gauss, [7], True),
-	#('adc_tra', guassian_gradient_magnitude, [5], True),
-	#('adc_tra', median, [7], True),
-	('adc_tra', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
-	('adc_tra', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
-	('adc_tra', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
-	#('adc_tra', centerdistance_xdminus1, [0], True),
-	#('adc_tra', centerdistance_xdminus1, [1], True),
-	#('adc_tra', centerdistance_xdminus1, [2], True)
+T1mprage = [
+	('T1mprage', intensities, [], False),
+	('T1mprage', local_mean_gauss, [3], True),
+	('T1mprage', local_mean_gauss, [5], True),
+	('T1mprage', local_mean_gauss, [7], True),
+	('T1mprage', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
+	('T1mprage', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
+	('T1mprage', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
 ]
 
-_t1 = [
-	('t1_sag_tfe', intensities, [], False),
-	('t1_sag_tfe', local_mean_gauss, [3], True),
-	('t1_sag_tfe', local_mean_gauss, [5], True),
-	('t1_sag_tfe', local_mean_gauss, [7], True),
-	#('t1_sag_tfe', guassian_gradient_magnitude, [5], True),
-	#('t1_sag_tfe', median, [7], True),
-	('t1_sag_tfe', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
-	('t1_sag_tfe', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
-	('t1_sag_tfe', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
-	#('t1_sag_tfe', centerdistance_xdminus1, [0], True),
-	#('t1_sag_tfe', centerdistance_xdminus1, [1], True),
-	#('t1_sag_tfe', centerdistance_xdminus1, [2], True)
+T1mprageCE = [
+	('T1mprageCE', intensities, [], False),
+	('T1mprageCE', local_mean_gauss, [3], True),
+	('T1mprageCE', local_mean_gauss, [5], True),
+	('T1mprageCE', local_mean_gauss, [7], True),
+	('T1mprageCE', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
+	('T1mprageCE', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
+	('T1mprageCE', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
 ]
 
-_t2 = [
-	('t2_sag_tse', intensities, [], False),
-	('t2_sag_tse', local_mean_gauss, [3], True),
-	('t2_sag_tse', local_mean_gauss, [5], True),
-	('t2_sag_tse', local_mean_gauss, [7], True),
-	#('t2_sag_tse', guassian_gradient_magnitude, [5], True),
-	#('t2_sag_tse', median, [7], True),
-	('t2_sag_tse', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
-	('t2_sag_tse', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
-	('t2_sag_tse', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
-	#('t2_sag_tse', centerdistance_xdminus1, [0], True),
-	#('t2_sag_tse', centerdistance_xdminus1, [1], True),
-	#('t2_sag_tse', centerdistance_xdminus1, [2], True)
+PD = [
+	('PD', intensities, [], False),
+	('PD', local_mean_gauss, [3], True),
+	('PD', local_mean_gauss, [5], True),
+	('PD', local_mean_gauss, [7], True),
+	('PD', local_histogram, [11, 'image', (0, 100), 5, None, None, 'ignore', 0], False), #11 bins, 5*2=10mm region
+	('PD', local_histogram, [11, 'image', (0, 100), 10, None, None, 'ignore', 0], False), #11 bins, 10*2=20mm region
+	('PD', local_histogram, [11, 'image', (0, 100), 15, None, None, 'ignore', 0], False) #11 bins, 15*2=30mm region
 ]
 
-features_to_extract = _flair # + _t1 + _t2 + _dw + _adc
+features_to_extract = T2 + FLAIR + T1mprage + T1mprageCE + PD
 
 
